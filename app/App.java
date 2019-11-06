160340bd8dec4aad3da48ba3ae09894a0161dc24
code2828 <50510911+code2828@users.noreply.github.com>
2019-11-06 16:10:52 +0800

    Add files via upload

--- app/App.java
+++ app/App.java
@@ -10,7 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-import app.exception.Stringexeptions;
+import app.exception.Stringexceptions;
 
 public class App extends JFrame implements ActionListener {
     private static final long serialVersionUID = 31828377563718273L;
@@ -19,19 +19,20 @@ public class App extends JFrame implements ActionListener {
     private static JTextArea text = new JTextArea();
     private static JButton button = new JButton("Guess!");
     private static JButton quit = new JButton("Quit game");
+    private static JButton section = new JButton("Change bounds");
     private static int ans;
-    private static int t = 10;
+    private static int t = 10,ub=0,lb=1000;
+    private static boolean set = false;
 
     public App() {
         super("Number Guessing Game");
-        ans = rd.nextInt() % 1000;
-        if (ans < 0) {
-            ans += 1000;
-        }
+        ans = rd.nextInt();
+        ans=map(ans);
         number.setBounds(40, 40, 200, 20);
         button.setBounds(260, 40, 100, 20);
         text.setBounds(40, 110, 200, 200);
-        quit.setBounds(150, 320, 100, 25);
+        quit.setBounds(50, 320, 125, 25);
+        section.setBounds(185, 320, 145, 25);
         text.setText("--- READ ME FIRST ---\nYou have 10 times to try.\nThe range is [0,1000).");
         text.setEditable(false);
         this.setLayout(null);
@@ -40,6 +41,7 @@ public class App extends JFrame implements ActionListener {
         this.add(button);
         this.add(text);
         this.add(quit);
+        this.add(section);
         this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 System.exit(-1);
@@ -47,6 +49,7 @@ public class App extends JFrame implements ActionListener {
         });
         button.addActionListener(this);
         quit.addActionListener(this);
+        section.addActionListener(this);
         this.setVisible(true);
     }
 
@@ -71,7 +74,7 @@ public class App extends JFrame implements ActionListener {
             }
             t--;
         }
-        if (e.getSource() == quit) {
+        else if (e.getSource() == quit) {
             int option = JOptionPane.showOptionDialog(this, "Are you sure you want to quit?", "Confirmation", 0, 0, null, null, null);
             if(option != 0)
             {
@@ -79,8 +82,32 @@ public class App extends JFrame implements ActionListener {
             }
             System.exit(0);
         }
+        else if (e.getSource() == section) {
+            if(set) {
+//                JOptionPane.showMessageDialog(this,"Already set!");
+            }
+            String opt = JOptionPane.showInputDialog(this,"Enter your desired upper bound.");
+            int option = strint(opt);
+            ub = option;
+            opt = JOptionPane.showInputDialog(this,"Enter you desired lower bound.");
+            option = strint(opt);
+            lb=option;
+            JOptionPane.showMessageDialog(this,"Bounds successfully set!");
+            set = true;
+        }
     }
 
+    public static int map(int n) {
+        if(n>ub)
+        {
+            n-=ub;
+        }
+        else if(n<lb)
+        {
+            n+=lb;
+        }
+        return n;
+    }
     public static int strint(String n) {
         int r = 0;
         for (int i = 0; i < n.length(); i++) {
